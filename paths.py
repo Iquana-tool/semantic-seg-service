ef0f3e53d7a24afb1511be29f5a2cbc9dd29c3fa
Robert Leist <robert.leist@dfki.de>
2025-07-02 12:56:54 +0200

    Add default paths for data, model, log, and job directories

--- paths.py
+++ paths.py
@@ -1,4 +1,7 @@
 import os
 
 
-DATA_PATH = os.environ.get("DATA_PATH")
+DATA_PATH = os.environ.get("DATA_PATH", "./data")
+MODEL_PATH = os.environ.get("MODEL_PATH", "./saved_models")
+LOG_PATH = os.environ.get("LOG_PATH", "./logs")
+JOBS_PATH = os.environ.get("JOBS_PATH", "./training_jobs")
