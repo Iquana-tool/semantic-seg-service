9563fd94048b9d2fb4049c01d34115f72b1de13e
Robert Leist <robert.leist@dfki.de>
2025-07-02 13:58:05 +0200

    Add endpoint to retrieve trained segmentation models

--- app/routes/models.py
+++ app/routes/models.py
@@ -1,7 +1,9 @@
+import os
+from collections import defaultdict
 from logging import getLogger
 from fastapi import APIRouter, UploadFile, File
 from models import MODEL_REGISTRY
-
+from paths import MODEL_PATH
 
 router = APIRouter("/models", tags=["models"])
 logger = getLogger(__name__)
@@ -17,3 +19,19 @@ async def get_trainable_models():
               for key, value in MODEL_REGISTRY.items()
               }
     return {"success": True, "models": models}
+
+
+@router.get("/get_trained_models")
+async def get_trained_models():
+    """Retrieve all trained segmentation models."""
+    logger.debug("Fetching trained segmentation models.")
+    # Get the trained models from the weights directory
+    trained_models = os.listdir(MODEL_PATH)
+    trained_models_dict = defaultdict(list)
+    for weight in trained_models:
+        name = weight.split(".")[0]
+        identifier, model_id = name.split("_")
+        trained_models_dict[identifier].append(model_id)
+    return {"success": True,
+            "message": f"Found {len(trained_models)} trained models.",
+            "models": trained_models_dict}
