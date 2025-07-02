e85d0003eecc05bb4d1bd18ed8a414e9a0999f8a
Robert Leist <robert.leist@dfki.de>
2025-07-02 12:57:05 +0200

    Add TrainingRequest model for training configuration and validation

---
+++ app/schemas/training.py
@@ -0,0 +1,47 @@
+import os
+
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional, Tuple
+
+from models import MODEL_REGISTRY
+from paths import DATA_PATH
+
+
+class TrainingRequest(BaseModel):
+    dataset_id: int
+    model_identifier: str = Field(default="unet", description="Identifier for the model to be trained. "
+                                                      "Should be got by GET /models/get_trainable_models")
+    model_id_db: int = Field(description="ID of the model in the database. Must be provided.")
+    restart: bool = Field(default=False, description="Whether to restart training from scratch. If set to True, "
+                                                     "the model will be trained from scratch, else it will continue "
+                                                     "training from the last checkpoint.")
+    epochs: int = Field(default=50, description="Number of epochs to train the model.")
+    batch_size: int = Field(default=64, description="Batch size to train the model.")
+    lr: float = Field(default=0.0001, description="Learning rate to train the model.")
+    augment: bool = Field(default=True, description="Whether to augment the dataset. This should be done for small "
+                                                    "datasets, but can be left out for bigger datasets.")
+    image_size: Optional[Tuple[int, int]] = Field(default=(256, 256), description="Image size to use. Smaller values "
+                                                                                  "may lead to faster training, "
+                                                                                  "but may also lead to "
+                                                                                  "loss of information.")
+    num_classes: int = Field(default=2, description="Number of classes that are present on the masks. Corresponds to the "
+                                                    "number of labels.")
+    in_channels: int = 3
+    early_stopping: bool = Field(default=True, description="Whether to use early stopping during training. "
+                                                           "This will stop training if the validation loss "
+                                                           "does not improve for 5 epochs.")
+
+    @field_validator('model_identifier')
+    def validate_model_identifier(cls, value):
+        if not value:
+            raise ValueError("Model identifier cannot be empty.")
+        if value not in MODEL_REGISTRY:
+            raise ValueError(f"Model identifier '{value}' is not recognized. "
+                             f"Available models: {list(MODEL_REGISTRY.keys())}.")
+        return value
+
+    @field_validator('dataset_id')
+    def validate_dataset_id(cls, value):
+        if str(value) not in os.listdir(DATA_PATH):
+            raise ValueError(f"Dataset with ID {value} does not exist in the data path {DATA_PATH}."
+                             f"Please make sure to upload the dataset first.")
