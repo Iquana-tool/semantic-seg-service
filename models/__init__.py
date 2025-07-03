7f8bc14b9177c666274c8a55bd085a7863da0ab5
Robert Leist <robert.leist@dfki.de>
2025-07-03 12:06:25 +0200

    Add model loading and metadata retrieval functions by ID

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,15 @@ import json
 import os
 
 import torch
-
+from logging import getLogger
+from paths import MODEL_PATH
 from .unet import get_unet
+
+logger = getLogger(__name__)
 
 MODEL_REGISTRY = {
     "unet": {
-        "getter": get_unet,
+        "getter": get_unet,  # Function to get the UNet model, absolutely required
         "description": "UNet model for image segmentation.",
         "speed": "Fast",
         "automatic tuning": False
@@ -34,6 +37,7 @@ def load_model_from_checkpoint_path(checkpoint_path: str, device: str = 'cpu', e
         raise FileNotFoundError(f"Model weights file {checkpoint_path} or config file {config_path} does not exist.")
     # Get the registration key and model ID from the file name
     reg_key, model_id = parse_weight_file_name(checkpoint_path)
+    logger.info(f"Loading {reg_key} model with ID: {model_id}")
     # Load the model using the registry
     model_fn = MODEL_REGISTRY.get(reg_key, {}).get("getter")
     with open(config_path, "r") as f:
@@ -44,6 +48,52 @@ def load_model_from_checkpoint_path(checkpoint_path: str, device: str = 'cpu', e
     if eval_mode:
         model.eval()
     return model, checkpoint
+
+
+def load_model_from_id(model_id: int, device: str = 'cpu', eval_mode=True) -> torch.nn.Module:
+    """
+    Load a model from its ID.
+
+    Args:
+        model_id (int): The ID of the model to load.
+        device (str): Device to load the model on, e.g., 'cpu' or 'cuda'.
+        eval_mode (bool): Whether to set the model in evaluation or train mode (i.e. gradients are not computed).
+
+    Returns:
+        tuple: (model, checkpoint) where model is the loaded model and checkpoint is the state dictionary for further
+        use.
+    """
+    weight_files = [f for f in os.listdir(MODEL_PATH) if f.endswith(".pt") and f.split("_")[-1].split(".")[0] == str(model_id)]
+    if not weight_files:
+        raise FileNotFoundError(f"No trained model found with ID {model_id}.")
+    if len(weight_files) > 1:
+        logger.warning(f"Multiple weight files found for model ID {model_id}. "
+                       f"Using the first one: {weight_files[0]}")
+    weight_file = os.path.join(MODEL_PATH, weight_files[0])
+    return load_model_from_checkpoint_path(weight_file, device=device, eval_mode=eval_mode)
+
+
+def load_metadata_from_id(model_id: int) -> dict:
+    """
+    Load metadata from a model ID.
+
+    Args:
+        model_id (int): The ID of the model to load metadata for.
+
+    Returns:
+        dict: Metadata dictionary containing model identifier and other details.
+    """
+    meta_files = [f for f in os.listdir(MODEL_PATH) if f.endswith(".json") and
+                    f.split("_")[-1].split(".")[0] == str(model_id)]
+    if not meta_files:
+        raise FileNotFoundError(f"No trained model found with ID {model_id}.")
+    if len(meta_files) > 1:
+        logger.warning(f"Multiple metadata jsons found for model ID {model_id}. "
+                       f"Using the first one: {meta_files[0]}")
+    meta_file = os.path.join(MODEL_PATH, meta_files[0])
+    with open(meta_file, "r") as f:
+        metadata = json.load(f)
+    return metadata
 
 
 def parse_weight_file_name(file_name: str):
