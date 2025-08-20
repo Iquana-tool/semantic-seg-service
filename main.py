2e6e68c651d42859265a2691a2289f479046883d
Robert Leist <robert.leist@dfki.de>
2025-08-20 07:42:11 +0200

    feat: added logging to a file

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import logging
 from app import create_app
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+# Set up logging
+logging.basicConfig(
+    filename="./logs.txt",
+    filemode='a',
+    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = create_app()
