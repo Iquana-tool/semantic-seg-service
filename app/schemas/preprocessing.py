abcb72c153b16f186c0ddfc0f9505cf5f09a91e1
Robert Leist <robert.leist@dfki.de>
2025-10-07 14:40:48 +0200

    Normalization is not part of the preprocessing anymore.

--- app/schemas/preprocessing.py
+++ app/schemas/preprocessing.py
@@ -1,14 +1,13 @@
 from typing import Optional
 
+import numpy as np
 import torch
 from pydantic import BaseModel, Field
 
 
 class Preprocessing(BaseModel):
-    normalize_mean_and_std: Optional[tuple[tuple[float, float, float], tuple[float, float, float]]] = Field(
-        default=None, description="Normalize by this mean and standard deviation. If None, does not normalize.")
+    """ Preprocessing is anything that should be done to the uploaded images exactly once. These are usually performance
+        heavy computations like Histogram equalization. This is a placeholder for now."""
 
-    def __call__(self, tensor: torch.Tensor) -> torch.Tensor:
-        if self.normalize:
-            tensor = torch.transforms.normalize(tensor, self.mean, self.std)
-        return tensor
+    def __call__(self, img: np.ndarray) -> np.ndarray:
+        return img
