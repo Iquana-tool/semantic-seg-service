17190975a16f91fdff564fe5da4b25cd6eb10e2b
Robert Leist <robert.leist@dfki.de>
2025-10-08 14:21:48 +0200

    Added new paths

--- paths.py
+++ paths.py
@@ -1,5 +1,8 @@
 import os
 
+BACKEND_URL = os.environ.get("BACKEND_URL", "localhost:8000")
+CELERY_URL = os.environ.get("CELERY_URL", "localhost:7001")
+
 DATA_PATH = os.environ.get("DATA_PATH", "./data")
 MODEL_WEIGHTS_PATH = os.environ.get("MODEL_PATH", "model_weights")
 MODEL_REGISTRY_ENTRY_PATHS = os.environ.get("MODEL_REGISTRY_ENTRY_PATHS", "./model_registry_entries")
