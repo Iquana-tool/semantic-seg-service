07b86140ed6c364df01c612db0d92994ebf45ae7
Robert Leist <robert.leist@dfki.de>
2025-10-10 09:17:53 +0200

    minor change

--- app/routes/training.py
+++ app/routes/training.py
@@ -4,6 +4,7 @@ from celery_tasks.training import train_model
 from app.state import MODEL_REGISTRY
 from app.schemas.training_run import TrainingRun, TrainingProgress, JobStatusEnum
 from celery_app import celery
+from celery.result import AsyncResult
 
 
 router = APIRouter(prefix="/training", tags=["training"])
@@ -31,15 +32,14 @@ async def start_training(req: TrainingRequest):
     # Update the training status
     training_run.set_status("training", JobStatusEnum.QUEUED, "Training is queued.")
 
-    task = train_model.delay(req.model_dump(), req.model_registry_key)
+    task: AsyncResult = train_model.delay(req.model_dump(), req.model_registry_key)
     training_run.task_id = task.id
 
     return {
         "success": True,
         "model_id": req.model_registry_key,
         "task_id": task.id,
-        "status": "In progress",
-        "message": "Training started in the background."
+        "message": "Training task enqueued."
     }
 
 
