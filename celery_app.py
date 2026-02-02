a21d0b1b6dc5cac50a3b215145d7a466af4c702c
Robert Leist <robert.leist@dfki.de>
2026-02-02 14:53:07 +0100

    updated celery name

--- celery_app.py
+++ celery_app.py
@@ -2,7 +2,7 @@ from celery import Celery
 from paths import REDIS_URL
 
 celery_app = Celery(
-    "iquana_celery", # Must match the name in your other services
+    "iquana_service_semantic_segmentation", # Must match the name in your other services
     broker=f"{REDIS_URL}/0",
     backend=f"{REDIS_URL}/1"
 )
